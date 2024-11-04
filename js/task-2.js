67934e35250bfdf4e76cb0d4f73d320eec7b0a18
Kostiantyn <konstantin_help@ukr.net>
2024-11-05 01:36:08 +0200

    add markup

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+function formatMessage(message, maxLength) {
+    const l = message.length;
+    if (l <= maxLength) {
+        return message;
+    } else {
+        message = message.slice(0, maxLength);
+        const dot = "..."
+        message = message + dot;
+        return message;
+    }
+}
+
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
